743842f179b4bdb1590b861218edd26d34c3e145
tiffanyandjia <104477148+tiffanyandjia@users.noreply.github.com>
2024-05-27 20:44:01 -0400

    Calculator

---
+++ src/main/java/test/tl/start/CalculatorTest.java
@@ -0,0 +1,26 @@
+package tl.start;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @org.junit.jupiter.api.Test
+    void init() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addNorthComponent() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addCenterButton() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void actionPerformed() {
+    }
+}
